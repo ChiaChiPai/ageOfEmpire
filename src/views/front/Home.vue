08933d2436f09d08b3ec30fdf1e78c2d04bca537
ChiaChiPai <csc981.04@gmail.com>
2020-03-13 23:46:45 +0800

    v2

--- src/views/front/Home.vue
+++ src/views/front/Home.vue
@@ -134,7 +134,7 @@ export default {
       vm.$http.get(api,{
         header:{
           "Access-Control-Allow-Origin": 'https://graph.facebook.com/',
-          "Access-Control-Allow-Methods": get,
+          "Access-Control-Allow-Methods": 'get',
           "Access-Control-Allow-Headers":'X-Requested-With'
         }
       }).then((response) => {
